0c63ce8a40c4fcce6b7cbca9bb1bfa6bbcae3401
jjherman94 <jjhf39@mst.edu>
2014-10-11 17:32:55 -0500

    Made the check winner function

--- server/game_app/match.py
+++ server/game_app/match.py
@@ -8,227 +8,279 @@ import os
 import itertools
 import scribe
 import jsonLogger
-import math
+import random
 
 Scribe = scribe.Scribe
 
-def loadClassDefaults(cfgFile = "config/defaults.cfg"):
-  cfg = networking.config.config.readConfig(cfgFile)
-  for className in cfg.keys():
-    for attr in cfg[className]:
-      setattr(eval(className), attr, cfg[className][attr])
+
+def loadClassDefaults(cfgFile="config/defaults.cfg"):
+    cfg = networking.config.config.readConfig(cfgFile)
+    for className in cfg.keys():
+        for attr in cfg[className]:
+            setattr(eval(className), attr, cfg[className][attr])
+
 
 class Match(DefaultGameWorld):
-  def __init__(self, id, controller):
-    self.id = int(id)
-    self.controller = controller
-    DefaultGameWorld.__init__(self)
-    self.scribe = Scribe(self.logPath())
-    if( self.logJson ):
-      self.jsonLogger = jsonLogger.JsonLogger(self.logPath())
-      self.jsonAnimations = []
-      self.dictLog = dict(gameName = "Plants", turns = [])
-    self.addPlayer(self.scribe, "spectator")
+    def __init__(self, id, controller):
+        self.id = int(id)
+        self.controller = controller
+        DefaultGameWorld.__init__(self)
+        self.scribe = Scribe(self.logPath())
+        if ( self.logJson ):
+            self.jsonLogger = jsonLogger.JsonLogger(self.logPath())
+            self.jsonAnimations = []
+            self.dictLog = dict(gameName="Plants", turns=[])
+        self.addPlayer(self.scribe, "spectator")
 
-    #TODO: INITIALIZE THESE!
-    self.mapWidth = None
-    self.mapHeight = None
-    self.turnNumber = None
-    self.maxPlants = None
-    self.playerID = None
-    self.gameNumber = id
+        # TODO: INITIALIZE THESE!
+        self.mapWidth = None
+        self.mapHeight = None
+        self.turnNumber = None
+        self.maxPlants = None
+        self.playerID = None
+        self.gameNumber = id
 
-  #this is here to be wrapped
-  def __del__(self):
-    pass
+    # this is here to be wrapped
+    def __del__(self):
+        pass
 
-  def dist(self, x1, y1, x2, y2):
-    return int(math.hypot(x1-x2, y1-y2))
+    def addPlayer(self, connection, type="player"):
+        connection.type = type
+        if len(self.players) >= 2 and type == "player":
+            return "Game is full"
+        if type == "player":
+            self.players.append(connection)
+            try:
+                self.addObject(Player, [connection.screenName, self.startTime])
+            except TypeError:
+                raise TypeError("Someone forgot to add the extra attributes to the Player object initialization")
+        elif type == "spectator":
+            self.spectators.append(connection)
+            # If the game has already started, send them the ident message
+            if (self.turn is not None):
+                self.sendIdent([connection])
+        return True
 
-  def addPlayer(self, connection, type="player"):
-    connection.type = type
-    if len(self.players) >= 2 and type == "player":
-      return "Game is full"
-    if type == "player":
-      self.players.append(connection)
-      try:
-        self.addObject(Player, [connection.screenName, self.startTime])
-      except TypeError:
-        raise TypeError("Someone forgot to add the extra attributes to the Player object initialization")
-    elif type == "spectator":
-      self.spectators.append(connection)
-      #If the game has already started, send them the ident message
-      if (self.turn is not None):
-        self.sendIdent([connection])
-    return True
+    def removePlayer(self, connection):
+        if connection in self.players:
+            if self.turn is not None:
+                winner = self.players[1 - self.getPlayerIndex(connection)]
+                self.declareWinner(winner, 'Opponent Disconnected')
+            self.players.remove(connection)
+        else:
+            self.spectators.remove(connection)
 
-  def removePlayer(self, connection):
-    if connection in self.players:
-      if self.turn is not None:
-        winner = self.players[1 - self.getPlayerIndex(connection)]
-        self.declareWinner(winner, 'Opponent Disconnected')
-      self.players.remove(connection)
-    else:
-      self.spectators.remove(connection)
+    def start(self):
+        if len(self.players) < 2:
+            return "Game is not full"
+        if self.winner is not None or self.turn is not None:
+            return "Game has already begun"
 
-  def start(self):
-    if len(self.players) < 2:
-      return "Game is not full"
-    if self.winner is not None or self.turn is not None:
-      return "Game has already begun"
+        # TODO: START STUFF
+        self.turn = self.players[-1]
+        self.turnNumber = -1
 
-    #TODO: START STUFF
-    self.turn = self.players[-1]
-    self.turnNumber = -1
-
-    self.nextTurn()
-    return True
+        self.nextTurn()
+        return True
 
 
-  def nextTurn(self):
-    self.turnNumber += 1
-    if self.turn == self.players[0]:
-      self.turn = self.players[1]
-      self.playerID = 1
-    elif self.turn == self.players[1]:
-      self.turn = self.players[0]
-      self.playerID = 0
+    def nextTurn(self):
+        self.turnNumber += 1
+        if self.turn == self.players[0]:
+            self.turn = self.players[1]
+            self.playerID = 1
+        elif self.turn == self.players[1]:
+            self.turn = self.players[0]
+            self.playerID = 0
 
-    else:
-      return "Game is over."
+        else:
+            return "Game is over."
 
-    for obj in self.objects.values():
-      obj.nextTurn()
+        for obj in self.objects.values():
+            obj.nextTurn()
 
-    self.checkWinner()
-    if self.winner is None:
-      self.sendStatus([self.turn] +  self.spectators)
-    else:
-      self.sendStatus(self.spectators)
-    
-    if( self.logJson ):
-      self.dictLog['turns'].append(
-        dict(
-          mapWidth = self.mapWidth,
-          mapHeight = self.mapHeight,
-          turnNumber = self.turnNumber,
-          maxPlants = self.maxPlants,
-          playerID = self.playerID,
-          gameNumber = self.gameNumber,
-          Players = [i.toJson() for i in self.objects.values() if i.__class__ is Player],
-          Mappables = [i.toJson() for i in self.objects.values() if i.__class__ is Mappable],
-          Plants = [i.toJson() for i in self.objects.values() if i.__class__ is Plant],
-          Mutations = [i.toJson() for i in self.objects.values() if i.__class__ is Mutation],
-          animations = self.jsonAnimations
-        )
-      )
-      self.jsonAnimations = []
+        self.checkWinner()
+        if self.winner is None:
+            self.sendStatus([self.turn] + self.spectators)
+        else:
+            self.sendStatus(self.spectators)
 
-    self.animations = ["animations"]
-    return True
+        if ( self.logJson ):
+            self.dictLog['turns'].append(
+                dict(
+                    mapWidth=self.mapWidth,
+                    mapHeight=self.mapHeight,
+                    turnNumber=self.turnNumber,
+                    maxPlants=self.maxPlants,
+                    playerID=self.playerID,
+                    gameNumber=self.gameNumber,
+                    Players=[i.toJson() for i in self.objects.values() if i.__class__ is Player],
+                    Mappables=[i.toJson() for i in self.objects.values() if i.__class__ is Mappable],
+                    Plants=[i.toJson() for i in self.objects.values() if i.__class__ is Plant],
+                    Mutations=[i.toJson() for i in self.objects.values() if i.__class__ is Mutation],
+                    animations=self.jsonAnimations
+                )
+            )
+            self.jsonAnimations = []
 
-  def checkWinner(self):
-    #TODO: Make this check if a player won, and call declareWinner with a player if they did
-    if self.turnNumber >= self.turnLimit:
-       self.declareWinner(self.players[0], "Because I said so, this shold be removed")
+        self.animations = ["animations"]
+        return True
+
+    def checkWinner(self):
+        # mother dead, mother health, most plants, lowest total rads,
+        # total strength, coin flip
+        motherDead1 = True  # true if player 1's mother plant is dead
+        motherDead2 = True  # true if player 2's mother plant is dead
+        random.seed()
+
+        # 0 = mother
+        for plant in self.objects.mutations:
+            if plant.owner == 0 and plant.rads <= plant.maxRad and plant.mutation == 0:
+                motherDead1 = False
+            if plant.owner == 1 and plant.rads <= plant.maxRad and plant.mutation == 0:
+                motherDead2 = False
+
+        if motherDead1:
+            self.declareWinner(self.players[1], "Player 1\'s mother plant is dead")
+        elif motherDead2:
+            self.declareWinner(self.players[0], "Player 2\'s mother plant is dead")
+        elif self.turnNumber >= self.turnLimit:
+            motherRad1 = 0
+            motherRad2 = 0
+            totalPlants1 = 0
+            totalPlants2 = 0
+            totalRads1 = 0
+            totalRads2 = 0
+            totalStrength1 = 0
+            totalStrength2 = 0
+            for plant in self.objects.mutations:
+                if plant.owner == 0:
+                    totalPlants1 += 1
+                    totalRads1 += plant.rads
+                    totalStrength1 += plant.strength
+                    if plant.mutation == 0:
+                        motherRad1 = plant.rads
+                if plant.owner == 1:
+                    totalPlants2 += 1
+                    totalRads2 += plant.rads
+                    totalStrength2 += plant.strength
+                    if plant.mutation == 0:
+                        motherRad2 = plant.rads
+            if motherRad1 < motherRad2:
+                self.declareWinner(self.players[0], "Player 1\'s mother plant has less rads")
+            elif motherRad1 > motherRad2:
+                self.declareWinner(self.players[1], "Player 2\'s mother plant has less rads")
+            elif totalPlants1 > totalPlants2:
+                self.declareWinner(self.players[0], "Player 1 has more plants")
+            elif totalPlants1 < totalPlants2:
+                self.declareWinner(self.players[1], "Player 2 has more plants")
+            elif totalRads1 < totalRads2:
+                self.declareWinner(self.players[0], "Player 1 has less total rads")
+            elif totalRads1 > totalRads2:
+                self.declareWinner(self.players[1], "Player 2 has less total rads")
+            elif totalStrength1 > totalStrength2:
+                self.declareWinner(self.players[0], "Player 1 has more total strength")
+            elif totalStrength1 < totalStrength2:
+                self.declareWinner(self.players[1], "Player 2 has more total strength")
+            else:
+                self.declareWinner(self.players[random.randint(0, 1)], "Coin flip since players are tied")
+
+    def declareWinner(self, winner, reason=''):
+        print "Player", self.getPlayerIndex(self.winner), "wins game", self.id
+        self.winner = winner
+
+        msg = ["game-winner", self.id, self.winner.user, self.getPlayerIndex(self.winner), reason]
+
+        if ( self.logJson ):
+            self.dictLog["winnerID"] = self.getPlayerIndex(self.winner)
+            self.dictLog["winReason"] = reason
+            self.jsonLogger.writeLog(self.dictLog)
+
+        self.scribe.writeSExpr(msg)
+        self.scribe.finalize()
+        self.removePlayer(self.scribe)
+
+        for p in self.players + self.spectators:
+            p.writeSExpr(msg)
+
+        self.sendStatus([self.turn])
+        self.playerID ^= 1
+        self.sendStatus([self.players[self.playerID]])
+        self.playerID ^= 1
+        self.turn = None
+        self.objects.clear()
+
+    def logPath(self):
+        return "logs/" + str(self.id)
+
+    @derefArgs(Player, None, None, None)
+    def germinate(self, object, x, y, mutation):
+        return object.germinate(x, y, mutation, )
+
+    @derefArgs(Plant, None)
+    def talk(self, object, message):
+        return object.talk(message, )
+
+    @derefArgs(Plant, None, None)
+    def radiate(self, object, x, y):
+        return object.radiate(x, y, )
+
+    @derefArgs(Plant, None, None)
+    def radiate(self, object, x, y):
+        return object.radiate(x, y, )
+
+    @derefArgs(Plant, None, None, None)
+    def uproot(self, object, x, y, mutation):
+        return object.uproot(x, y, mutation, )
 
 
-  def declareWinner(self, winner, reason=''):
-    print "Player", self.getPlayerIndex(self.winner), "wins game", self.id
-    self.winner = winner
+    def sendIdent(self, players):
+        if len(self.players) < 2:
+            return False
+        list = []
+        for i in itertools.chain(self.players, self.spectators):
+            list += [[self.getPlayerIndex(i), i.user, i.screenName, i.type]]
+        for i in players:
+            i.writeSExpr(['ident', list, self.id, self.getPlayerIndex(i)])
 
-    msg = ["game-winner", self.id, self.winner.user, self.getPlayerIndex(self.winner), reason]
-    
-    if( self.logJson ):
-      self.dictLog["winnerID"] =  self.getPlayerIndex(self.winner)
-      self.dictLog["winReason"] = reason
-      self.jsonLogger.writeLog( self.dictLog )
-    
-    self.scribe.writeSExpr(msg)
-    self.scribe.finalize()
-    self.removePlayer(self.scribe)
+    def getPlayerIndex(self, player):
+        try:
+            playerIndex = self.players.index(player)
+        except ValueError:
+            playerIndex = -1
+        return playerIndex
 
-    for p in self.players + self.spectators:
-      p.writeSExpr(msg)
-    
-    self.sendStatus([self.turn])
-    self.playerID ^= 1
-    self.sendStatus([self.players[self.playerID]])
-    self.playerID ^= 1
-    self.turn = None
-    self.objects.clear()
-
-  def logPath(self):
-    return "logs/" + str(self.id)
-
-  @derefArgs(Player, None, None, None)
-  def germinate(self, object, x, y, mutation):
-    return object.germinate(x, y, mutation, )
-
-  @derefArgs(Plant, None)
-  def talk(self, object, message):
-    return object.talk(message, )
-
-  @derefArgs(Plant, None, None)
-  def radiate(self, object, x, y):
-    return object.radiate(x, y, )
-
-  @derefArgs(Plant, None, None)
-  def radiate(self, object, x, y):
-    return object.radiate(x, y, )
-
-  @derefArgs(Plant, None, None, None)
-  def uproot(self, object, x, y, mutation):
-    return object.uproot(x, y, mutation, )
+    def sendStatus(self, players):
+        for i in players:
+            i.writeSExpr(self.status())
+            i.writeSExpr(self.animations)
+        return True
 
 
-  def sendIdent(self, players):
-    if len(self.players) < 2:
-      return False
-    list = []
-    for i in itertools.chain(self.players, self.spectators):
-      list += [[self.getPlayerIndex(i), i.user, i.screenName, i.type]]
-    for i in players:
-      i.writeSExpr(['ident', list, self.id, self.getPlayerIndex(i)])
+    def status(self):
+        msg = ["status"]
 
-  def getPlayerIndex(self, player):
-    try:
-      playerIndex = self.players.index(player)
-    except ValueError:
-      playerIndex = -1
-    return playerIndex
+        msg.append(
+            ["game", self.mapWidth, self.mapHeight, self.turnNumber, self.maxPlants, self.playerID, self.gameNumber])
 
-  def sendStatus(self, players):
-    for i in players:
-      i.writeSExpr(self.status())
-      i.writeSExpr(self.animations)
-    return True
+        typeLists = []
+        typeLists.append(["Player"] + [i.toList() for i in self.objects.values() if i.__class__ is Player])
+        typeLists.append(["Mappable"] + [i.toList() for i in self.objects.values() if i.__class__ is Mappable])
+        typeLists.append(["Plant"] + [i.toList() for i in self.objects.values() if i.__class__ is Plant])
+        updated = [i for i in self.objects.values() if i.__class__ is Mutation and i.updatedAt > self.turnNumber - 3]
+        if updated:
+            typeLists.append(["Mutation"] + [i.toList() for i in updated])
 
+        msg.extend(typeLists)
 
-  def status(self):
-    msg = ["status"]
+        return msg
 
-    msg.append(["game", self.mapWidth, self.mapHeight, self.turnNumber, self.maxPlants, self.playerID, self.gameNumber])
-
-    typeLists = []
-    typeLists.append(["Player"] + [i.toList() for i in self.objects.values() if i.__class__ is Player])
-    typeLists.append(["Mappable"] + [i.toList() for i in self.objects.values() if i.__class__ is Mappable])
-    typeLists.append(["Plant"] + [i.toList() for i in self.objects.values() if i.__class__ is Plant])
-    updated = [i for i in self.objects.values() if i.__class__ is Mutation and i.updatedAt > self.turnNumber-3]
-    if updated:
-      typeLists.append(["Mutation"] + [i.toList() for i in updated])
-
-    msg.extend(typeLists)
-
-    return msg
-
-  def addAnimation(self, anim):
-    # generate the sexp
-    self.animations.append(anim.toList())
-    # generate the json
-    if( self.logJson ):
-      self.jsonAnimations.append(anim.toJson())
-  
+    def addAnimation(self, anim):
+        # generate the sexp
+        self.animations.append(anim.toList())
+        # generate the json
+        if ( self.logJson ):
+            self.jsonAnimations.append(anim.toJson())
 
 
 loadClassDefaults()
