64c270cac3463722f5b486a8e433f3c640b9508f
BryceMehring <brycemehring@gmail.com>
2014-11-07 17:15:04 -0600

    Added noise texture.

--- plugins/plants/resources.r
+++ plugins/plants/resources.r
@@ -6,12 +6,14 @@ texture mother textures/mothershade.png
 texture soaker textures/soakershade.png
 texture aralia textures/aralia1.png
 texture spawner textures/dandelion.png
-texture grid textures/ground.jpg
-texture rad_pool textures/rad_pool.png
 texture bumbleweed1 textures/bumbleweed1.png
 texture bumbleweed2 textures/bumbleweed2.png
 texture choke1 textures/choke1.png
 texture choke2 textures/choke2.png
 texture seed textures/seed.png
+
+texture grid textures/ground.jpg
+texture noise textures/noise.png
+texture rad_pool textures/rad_pool.png
 
 animation aralia_anim textures/animations/araliaSpriteSheet.png
