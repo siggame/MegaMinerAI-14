04bbe1872d3dde9ada91d3ac1a0e5521c35b5a13
Trey Nickelsen <jtng28@mst.edu>
2014-11-07 19:54:02 -0600

    Account for rage factor

--- server/game_app/objects.py
+++ server/game_app/objects.py
@@ -186,7 +186,7 @@ class Plant(Mappable):
       return 'Turn {}: Your {} does not have any radiates left.'.format(self.game.turnNumber, self.id)
     elif not (0 <= x < self.game.mapWidth) or not (0 <= y < self.game.mapHeight):
       return 'Turn {}: Your {} cannot radiate off the map.'.format(self.game.turnNumber, self.id)
-    elif not (self.game.dist(self.x, self.y, x, y) < self.range):
+    elif not (self.game.dist(self.x, self.y, x, y) <= self.range):
       return 'Turn {}: Your {} cannot radiate outside of its range.'.format(self.game.turnNumber, self.id)
 
     target_plant = None
